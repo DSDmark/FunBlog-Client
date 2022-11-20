e26c9d75d2402a8e699cc16b7921c6e69d28c27e
Sanskar Sahu <sahup3296@gmail.com>
2022-11-20 09:36:22 +0000

    LOGIN WAS COMPLETED

---
+++ src/redux/types/authTypes.ts
@@ -0,0 +1,15 @@
+import { IUser } from "../../utils/TypeScript"
+
+export const AUTH = "AUTH";
+
+
+export interface IAuth {
+	token?: string;
+	user?: IUser;
+}
+
+export interface IAuthType {
+	type: typeof AUTH;
+	payload: IAuth;
+}
+
--- src/utils/TypeScript.tsx
+++ src/utils/TypeScript.tsx
@@ -1,9 +1,18 @@
-import {ChangeEvent,FormEvent} from "react";
+import { ChangeEvent, FormEvent } from "react";
 
 export type InputChange = ChangeEvent<HTMLInputElement>
 export type FormSubmit = FormEvent<HTMLFormElement>
 
-export interface IUserLogin{
-	account:string;
-	password:string;
+export interface IUserLogin {
+	account: string;
+	password: string;
+}
+
+export interface IUser extends IUserLogin {
+	name: string;
+	avatar: string;
+	role: string;
+	type: string;
+	updateAt: string;
+	_id: string;
 }
